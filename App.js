8a52634dad817daccde0de37b07d19116328fbb4
Hanan <hanan.hf2002@hotmail.com>
2025-11-25 22:40:00 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+
+export default function App() {
+  return(
+        <View>
+            <Text style={{padding: 10}}>Hello world!</Text>
+        </View>
+
+  );
+}
+
